9c405002d991c85ab5515b2041227568141b8148
Jensen-Albin-Sander <51294735+Jensen-Albin-Sander@users.noreply.github.com>
2020-01-10 21:25:56 +0100

    Fixed temp and celsius

--- maps/script/darksky.js
+++ maps/script/darksky.js
@@ -2,6 +2,10 @@ window.addEventListener('load', () => {
     let long;
     let lat;
 
+    let tempCelsius = document.querySelector(".Celsius");
+    let temperatureInfo = document.querySelector(".Info");
+    let locationTimeZone = document.querySelector(".timezone");
+
     
 
 
@@ -11,7 +15,7 @@ window.addEventListener('load', () => {
             lat = position.coords.latitude;
 
             const proxy = "https://cors-anywhere.herokuapp.com/";
-            const apiCall = proxy + "https://api.darksky.net/forecast/d3c612489c14f20b69123b8d0828d0f8/" + lat + "," + long;
+            const apiCall = proxy + "https://api.darksky.net/forecast/d3c612489c14f20b69123b8d0828d0f8/" + lat + "," + long + "?units=si";
 
             fetch(apiCall)
             .then(function(response) {
@@ -19,6 +23,10 @@ window.addEventListener('load', () => {
             })
             .then(function(data) {
                 console.log(data);
+                const {temperature, currently, timezone} = data.currently;
+                tempCelsius.textContent = temperature;
+                temperatureInfo.textContent = currently;
+                locationTimeZone.textContent = data.timezone;
             });
 
         });
